5962dca2c3aa12758277102a299516b4166a968c
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-07-06 09:58:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import torch
 
 st.header("Object Detection using YOLOv5s")
 
-model = torch.hub.load("ultralytics/yolov5", "yolov5s", pretrained=True)
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5s.pt', source='github') 
 
 img = st.file_uploader("Upload an image.", type=['png','jpg'])
 
