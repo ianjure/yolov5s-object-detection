5638b69a4a108714566a9d159ddd12368dc4335b
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-07-06 09:08:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 import torch
-import cv2
 
 st.header("Object Detection using YOLOv5s")
 
-model = torch.hub.load('yolov5', 'yolov5s', source='local')
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', source='github')
 
 img = st.file_uploader("Upload an image.", type=['png','jpg'])
 
 if img is not None:
     PIL_image = Image.open(img).convert("RGB")
     image = np.asarray(PIL_image)
-    #corrected_colors = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  
 
     results = model(image)
 
