6e8352f44e6ace9bb2fc01be3c6a5de394740db3
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-07-06 10:18:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,27 @@ import numpy as np
 from PIL import Image
 import torch
 
+CFG_MODEL_PATH = "models/yourModel.pt"
+CFG_ENABLE_URL_DOWNLOAD = True
+CFG_ENABLE_VIDEO_PREDICTION = True
+if CFG_ENABLE_URL_DOWNLOAD:
+    # Configure this if you set cfg_enable_url_download to True
+    url = "https://archive.org/download/yoloTrained/yoloTrained.pt"
+# End of Configurations
+
 st.header("Object Detection using YOLOv5s")
 
-model = torch.hub.load('ultralytics/yolov5', 'yolov5s', source='github') 
+@st.cache_resource
+def loadmodel():
+    if CFG_ENABLE_URL_DOWNLOAD:
+        CFG_MODEL_PATH = f"models/{url.split('/')[-1:][0]}"
+    model = torch.hub.load('ultralytics/yolov5', 'custom', path=CFG_MODEL_PATH, force_reload=True)
+    return model
 
 img = st.file_uploader("Upload an image.", type=['png','jpg'])
 
 if img is not None:
+    model = loadmodel()
     PIL_image = Image.open(img).convert("RGB")
     image = np.asarray(PIL_image)
 
